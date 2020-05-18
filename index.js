1c8a7dc78e8e2af2bca96b86e5472b119e85ec35
Ivan Luu <ivluu@ucsd.edu>
2020-05-18 02:58:56 -0700

    save

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.slick').slick({
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    draggable: true,
+    fade: true
+  });
+});
+
+$('.navbar-toggler').on("click", function(){
+    $('#pushContent').slideToggle();
+});
