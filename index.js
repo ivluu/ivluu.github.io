8192af2f99b62545c6f7cad724980dd8397bdd66
Ivan Luu <ivluu@ucsd.edu>
2020-05-18 04:13:32 -0700

    update index.css, index.html, index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+
+let bday = new Date(1996, 10, 27);
+let diff_ms = Date.now() - bday.getTime();
+let age_dt = new Date(diff_ms);
+let age = Math.abs(age_dt.getUTCFullYear() - 1970);
+
 $(document).ready(function() {
+
+  myInfo.innerHTML = "<strong>Name:</strong> Ivan Luu</br><strong>Age: </strong>" + age + "</br><strong>Born in: </strong>Los Angeles, CA</br><strong>Lives in: </strong>Rancho Palos Verdes, California</br><strong>Ethnicity:</strong> Chinese</br><strong>Nationality:</strong> American</br><strong>Languages:</strong> English, Cantonese</br>";
   $('.slick').slick({
     dots: true,
     arrows: true,
