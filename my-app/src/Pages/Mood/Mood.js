2dea0f36fe74403235e3d1dbe676b787662d53ce
ivluu <ivluu@ucsd.edu>
2022-12-13 14:33:45 -0800

    change spacing hopefully

--- my-app/src/Pages/Mood/Mood.js
+++ my-app/src/Pages/Mood/Mood.js
@@ -13,10 +13,14 @@ export default function Mood() {
                     <br></br> */}
                     <h2>Motivation</h2>
                     <p>
-                        Growing up an only child and having to learn and discover things on my own is a big reason why I am more intrigued by things than people.
-                        There is nothing more satisfying to me than taking things apart and putting them back together just to see how they work. I'm the type of person
-                        who would rather build things I want, rather than buy them. Every desktop I have ever owned was built on my own, along with all the table tennis
-                        paddles in my collection. I am also a huge car enthusiast, hoping to one day build a car that I can destroy and rebuild constantly. 
+                        Growing up an only child and having to obtain most knowledge on my own is a big reason why I am more intrigued by things than people. With very few
+                        resources to guide me, I was forced to learn mostly from my mistakes, avoiding shortcuts because I didn't know of any. There is nothing more satisfying
+                        to me than taking something apart and putting it back together just to see how it works. If that something breaks, then I just have another mistake I need
+                        to learn from. I'm the type of person who would rather build things than buy things. Every desktop I have ever owned was built on my own, along with all the
+                        table tennis paddles in my collection. I am also a huge car enthusiast, hoping to one day build a car that I can destroy and rebuild constantly. Overtime,
+                        I feel like the numerous mistakes I've made when exploring a topic hands-on have given me a better understanding of them than if I were to learn about them
+                        from someone else. Thankfully, I have parents who blindly support me because they grew up in a time where my interests, hobbies, and choice of career path 
+                        were less relevant. So, if I'm not striving to succeed for myself, I'll at least do it for them.
                     </p>
                     <br></br>
                     <h2>Inspiration</h2>
